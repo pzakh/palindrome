20e7e053fffce700514078087a874085a43b5613
Pavel Zakharau <pavel.zakharau@sqil.by>
2015-02-04 00:47:28 +0300

    Add working time for method calculated Palindrome in milliseconds

--- src/main/java/by/hrodna/pzakh/palindrome/Palindrome.java
+++ src/main/java/by/hrodna/pzakh/palindrome/Palindrome.java
@@ -10,30 +10,29 @@ import java.math.BigInteger;
  */
 public class Palindrome {
     static final Log log = LogFactory.getLog(Palindrome.class);
-    private int n;
-
-   /* public Palindrome(int n) {
-        this.n = n;
-    }*/
 
     public String getResult(int n) {
+        long startTime = System.nanoTime();
         String result = findMax(n);
+        long stopTime = System.nanoTime() - startTime;
+        log.info(stopTime / 1e9 + " time working in seconds");
         return result;
     }
+
     private String findMax(int n) {
         int maxNumber = (int) Math.pow(10, n) - 1;
         String multiplyToString = null;
         for (int i = maxNumber; i > 0; i--) {
             BigInteger multiplyResult = BigInteger.valueOf(maxNumber).multiply(BigInteger.valueOf(i));
             int multiplyLength = String.valueOf(multiplyResult).length();
-            multiplyToString= String.valueOf(multiplyResult);
+            multiplyToString = String.valueOf(multiplyResult);
             int k = 0, count = 0;
             for (; k < multiplyLength / 2; k++)
                 if (multiplyToString.charAt(k) == multiplyToString.charAt(multiplyLength - k - 1))
                     count++;
             if (count == k) {
                 if (multiplyToString.equals(new StringBuilder(multiplyToString).reverse().toString())) {
-                    log.info(multiplyToString + " is a palindrome " + maxNumber + " * " + i + " == " + multiplyResult);
+                    log.info("For n = " + n + " calculated result " + multiplyToString + "(" + maxNumber + " * " + i + " = " + multiplyResult + ")");
                 } else {
                     log.info("Palindrome is not found ");
                 }
